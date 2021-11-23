1daded1f80013100eeff541a5ea497dd69c33115
Maneira3232 <93150997+Maneira3232@users.noreply.github.com>
2021-11-23 18:57:22 -0500

    changes

--- Assets/script.js
+++ Assets/script.js
@@ -6,7 +6,7 @@ var timeBlock = $(".time-block")
 var currentTime = moment().hours()
 
 function setColor() {
-    timeBlock.each(function() {
+    timeBlock.each(function () {
         var hour = $(this).attr("id")
         if (currentTime > hour) {
             $(this).addClass("past")
@@ -22,5 +22,23 @@ function setColor() {
         }
     })
 }
+
+$(".saveBtn").on("click", function () {
+    var value = $(this).siblings('.description').val()
+    //console.log(value)
+    var time = $(this).attr('id')
+    //console.log(time)
+    localStorage.setItem(time, value)
+})
+
+$("#hour9").val(localStorage.getItem('9'))
+$("#hour10").val(localStorage.getItem('10'))
+$("#hour11").val(localStorage.getItem('11'))
+$("#hour12").val(localStorage.getItem('12'))
+$("#hour13").val(localStorage.getItem('13'))
+$("#hour14").val(localStorage.getItem('14'))
+$("#hour15").val(localStorage.getItem('15'))
+$("#hour16").val(localStorage.getItem('16'))
+$("#hour17").val(localStorage.getItem('17'))
 
 setColor()
